74e4ae54978de1fb7aa0b5a087e337d2fbdb03c6
April Lindie <april@MacBookPro.lan>
2026-05-21 20:29:54 -0400

    Complete earthquake data lab

--- server/app.py
+++ server/app.py
@@ -20,7 +20,36 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+
+def earthquake_to_dict(earthquake):
+    return {
+        "id": earthquake.id,
+        "magnitude": earthquake.magnitude,
+        "location": earthquake.location,
+        "year": earthquake.year,
+    }
+
+
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+
+    if not earthquake:
+        body = {"message": f"Earthquake {id} not found."}
+        return make_response(body, 404)
+
+    return make_response(earthquake_to_dict(earthquake), 200)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    body = {
+        "count": len(quakes),
+        "quakes": [earthquake_to_dict(quake) for quake in quakes],
+    }
+
+    return make_response(body, 200)
 
 
 if __name__ == '__main__':
